3389a628166effcbc30881ee13c64b2ebe1b1c69
MehrabAkram <81154917+MehrabAkram@users.noreply.github.com>
2023-01-18 21:49:43 +0600

    rtu

--- wp-content/themes/alpha/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function alpha_bootstrapping(){
-    load_theme_textdomain("alpha");
-    add_theme_support("post_thumbnail");
-    add_theme_support("title-tag");
-}
-
-add_action("after_setup_theme","alpha_bootstrapping");
